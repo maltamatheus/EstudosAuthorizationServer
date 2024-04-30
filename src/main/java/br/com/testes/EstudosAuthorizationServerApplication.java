28e7aa22ae34e317f953ab679c466ca7bdbfbb13
Matheus Malta de Aguiar <maltamatheus@gmail.com>
2024-04-30 17:31:10 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/testes/EstudosAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.testes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosAuthorizationServerApplication.class, args);
+	}
+
+}
