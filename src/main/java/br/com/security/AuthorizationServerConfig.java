30c3de208ca76c1a64cea173efdc0828364bbb54
Matheus Malta de Aguiar <maltamatheus@gmail.com>
2024-04-30 22:35:15 -0300

    Incluso AuthorizationServerConfig

--- src/main/java/br/com/security/AuthorizationServerConfig.java
+++ src/main/java/br/com/security/AuthorizationServerConfig.java
@@ -1,13 +1,37 @@
 package br.com.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
-
 	
-
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		System.out.println("Configure ClientDetailsServiceConfigurer");
+		clients.inMemory()
+			.withClient("cliente")
+			.secret(passwordEncoder.encode("senha"))
+			.authorizedGrantTypes("password")
+			.scopes("write","read");
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		System.out.println("Configure AuthorizationServerEndpointsConfigurer");
+		endpoints.authenticationManager(authenticationManager);
+	}
 }
